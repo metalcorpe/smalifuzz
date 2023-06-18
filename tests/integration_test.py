b611371c7f3c6572e82cdeca8e617560023b48a8
Tavaninja <metalcorpe@gmail.com>
2023-06-18 16:29:05 +0300

    more path trials

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -36,7 +36,7 @@ class IntegrationTestSuite(unittest.TestCase):
 
     def test_apktool_unpack_success(self):
         apktool_path = fetch_apktool()
-        apk_path = Path(r"tests\test_vectors\app-release0.apk").absolute()
+        apk_path = Path("tests") / "test_vectors" / "app-release0.apk"
         with tempfile.TemporaryDirectory(
             suffix=None, prefix="tmp_", dir=".", ignore_cleanup_errors=False
         ) as tmpdirname:
